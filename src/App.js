3cf979dbd6fddd5b77b3e7e985c1232000eb89e8
roodhouse <rughjm@gmail.com>
2023-08-03 10:40:18 -0500

    add comment mbView done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import Comment from './components/Comment'
+import Add from './components/Add';
+
+// comment card
+// add comment card
+// delete module
 
 function App() {
   return (
     <div className="App" id='app'>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <div id='mainWrapper'>
+        <div id='mainContainer' className='bg-veryLightGray px-4 py-8'>
+          <div id='commentWrapper'>
+            <Comment />
+          </div>
+          <div id='addCommentWrapper'>
+            <Add />
+          </div>
+          {/* <div id='delete'>
+            <Delete />
+          </div> */}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Add.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Data from '../data.json'
+
+function Add() {
+
+  return (
+    <>
+        <div id="addCommentContainer" className='bg-white rounded-lg p-4'>
+            <div id="formContainer">
+                <form noValidate>
+                    <div id="textContainer" className='mb-4 rounded-lg border border-lightGray pt-3 pl-6'>
+                        <textarea name="newComment" id="newComment" placeholder='Add a comment...' cols="30" rows="3" className='placeholder:text-base placeholder:font-normal placeholder:text-grayishBlue'></textarea>
+                    </div>
+                    <div id="iconSendContainer" className='flex justify-between items-center'>
+                        <div id="iconContainer" className='w-8 h-8'>
+                            <img src={Data.currentUser.image.webp} alt="avatar" />
+                        </div>
+                        <div id="submitContainer" className='bg-moderateBlue text-white rounded-lg py-3 px-[30px]'>
+                            <button type='submit'>SEND</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Add
